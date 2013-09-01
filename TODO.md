0c47e30fafe9307c152f4bf6d15fc58755e51f42
Ben Scofield <git@turrean.com>
2013-09-01 16:33:15 -0400

    Show top-level keys on the homepage

--- TODO.md
+++ TODO.md
@@ -1,6 +1,1 @@
-* Vagrant box
-  * nginx
-	* app dir
-* Bootstrap 3
-* Angular.js 
 * hit etcd for data / updates
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,35 @@
-App = Ember.Application.create();
+Wetcd = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+Wetcd.Router.map(function() {
+  this.route('keys');
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+Wetcd.Etcd = Ember.Object.extend({});
+Wetcd.Etcd.reopenClass({
+  keys: function(key) {
+    var path = '';
+    if (key != '') { path = key; }
+
+    var keys = Em.A();
+
+    $.get("http://192.168.10.10/v1/keys/" + path, function(response) {
+      $.parseJSON(response).forEach(function (k) {
+        keys.pushObject(k);
+      });
+    });
+    return keys;
+  }
+});
+
+
+Wetcd.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('keys');
+  }
+});
+
+Wetcd.KeysRoute = Ember.Route.extend({
+  model: function(params) {
+    return Wetcd.Etcd.keys('');
   }
 });
