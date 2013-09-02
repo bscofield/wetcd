eb9889c50209dc594383909b1b9f9fe07f947918
Ben Scofield <git@turrean.com>
2013-09-01 22:39:35 -0400

    Start browsing down the key hierarchy

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,29 @@
 Wetcd = Ember.Application.create();
 
 Wetcd.Router.map(function() {
-  this.route('keys');
+  this.resource('keys');
+  this.resource('key', { path: '/keys/*key' });
 });
 
 Wetcd.Etcd = Ember.Object.extend({});
 Wetcd.Etcd.reopenClass({
   keys: function(key) {
     var path = '';
-    if (key != '') { path = key; }
+    if (key != '') { path = key.replace( new RegExp("^\/+"), ''); }
 
-    var keys = Em.A();
+    var list = Em.A();
 
     $.get("http://192.168.10.10/v1/keys/" + path, function(response) {
-      $.parseJSON(response).forEach(function (k) {
-        keys.pushObject(k);
-      });
+      var data = $.parseJSON(response);
+      if (Ember.isArray(data)) {
+        data.forEach(function (k) {
+          list.pushObject(k);
+        });
+      } else {
+        list.pushObject(data);
+      }
     });
-    return keys;
+    return list;
   }
 });
 
@@ -31,5 +37,16 @@ Wetcd.IndexRoute = Ember.Route.extend({
 Wetcd.KeysRoute = Ember.Route.extend({
   model: function(params) {
     return Wetcd.Etcd.keys('');
+  }
+});
+
+Wetcd.KeyRoute = Ember.Route.extend({
+  model: function(params) {
+    console.log(params.key);
+    return Wetcd.Etcd.keys(params.key);
+  },
+
+  renderTemplate: function() {
+    this.render('keys');
   }
 });
